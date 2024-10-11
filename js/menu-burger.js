468471e87ee84b54ae52ddc55de31ceea377deb6
Chally <carlosgilino@gmail.com>
2024-10-11 18:38:21 +0200

    create new function to hidde open burger menu on page resize

--- js/menu-burger.js
+++ js/menu-burger.js
@@ -3,25 +3,34 @@ console.log('Menu burger');
 const menuDesplegado = document.getElementById('menu-desplegado')
 
 
+const handleMenu = () => {
 
-const handleMenu = ()=>{
-
-    
-
-    if(menuDesplegado.classList.contains('top-[-200px]')){
+    if (menuDesplegado.classList.contains('top-[-200px]')) {
         console.log('MENU HIDDEN');
         menuDesplegado.classList.remove('top-[-200px]')
         menuDesplegado.classList.add('top-[40px]')
         console.log(menuDesplegado.classList.value);
-        
-        
-    }else{
+
+
+    } else {
         console.log('MENU VISIBLE');
         menuDesplegado.classList.remove('top-[40px]')
         menuDesplegado.classList.add('top-[-200px]')
-        
-    }
-    
 
+    }
 
 }
+
+const closeBurgerMenuOnResize = () => {
+    const windowWidth = window.innerWidth;
+
+    // Cierra el menu burger si al aumentar el ancho de la pantalla 
+    // este se encuentra abierto
+
+    if (windowWidth > 1024 && !menuDesplegado.classList.contains('top-[-200px]')) {
+        menuDesplegado.classList.remove('top-[40px]')
+        menuDesplegado.classList.add('top-[-200px]')
+    }
+}
+
+window.addEventListener("resize", closeBurgerMenuOnResize);
