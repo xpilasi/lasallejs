9a37dde3dc36c9a20da90529462a263d4d2e8fe8
Mary Quiroz <maryqr21@gmail.com>
2024-10-18 19:49:27 +0200

    add fechas.js

---
+++ js/fechas.js
@@ -0,0 +1,18 @@
+function updateDateTime() {
+    const now = new Date();
+    const options = { 
+        weekday: 'long', 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric', 
+        hour: '2-digit', 
+        minute: '2-digit', 
+        hour12: false
+    };
+    const formattedDateTime = now.toLocaleDateString('es-ES', options);
+    document.getElementById('date-time').innerText = formattedDateTime;
+}
+
+
+updateDateTime();
+
