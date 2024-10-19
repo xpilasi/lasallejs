7e07d12c0d30467f70a756b3321eabb586246f09
Chally <carlosgilino@gmail.com>
2024-10-19 14:50:53 +0200

    add form notification modal

--- js/form.js
+++ js/form.js
@@ -1,3 +1,6 @@
+const popUpWindow = document.getElementById('popup');
+
+
 const enviarFormulario = (event) => {
   event.preventDefault();  // Evita que el formulario se envíe hasta que se validen los campos.
 
@@ -8,13 +11,14 @@ const enviarFormulario = (event) => {
   // Validar todos los campos
   let esValido = true;
   esValido &= validarCampo(nameForm, /^[a-z ,.'-]+$/i);
-  esValido &= validarCampo(lastNameForm, /^[a-z ,.'-]+$/i); 
-  esValido &= validarCampo(emailForm, /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/); 
+  esValido &= validarCampo(lastNameForm, /^[a-z ,.'-]+$/i);
+  esValido &= validarCampo(emailForm, /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
 
   if (esValido) {
     console.log("Formulario válido. Enviando datos...");
     // enviar los datos al servidor, o dejar que el formulario se envíe
-    event.target.submit(); // Solo si es válido se permite el envío
+    showConfirmationPopUp()
+    // event.target.submit(); // Solo si es válido se permite el envío  <-- Lo cancelo porque si se envia, no se ve el popup
   } else {
     console.log("Formulario inválido. Revisa los campos.");
   }
@@ -35,13 +39,34 @@ const validarCampo = (campo, regex) => {
 
 // Validación en tiempo real con 'keyup' para cada campo
 document.querySelector("#name-form").addEventListener("keyup", (event) => {
-  validarCampo(event.target, /^[a-z ,.'-]+$/i); 
+  validarCampo(event.target, /^[a-z ,.'-]+$/i);
 });
 
 document.querySelector("#last-name-form").addEventListener("keyup", (event) => {
-  validarCampo(event.target, /^[a-z ,.'-]+$/i); 
+  validarCampo(event.target, /^[a-z ,.'-]+$/i);
 });
 
 document.querySelector("#email-form").addEventListener("keyup", (event) => {
   validarCampo(event.target, /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
 });
+
+
+// Mostrar ventana emergente de confirmacion
+
+
+function showConfirmationPopUp() {
+
+  console.log(`Has pedido que se mostrara el popup de confirmacion. El elemento DOM de la ventana es: ${popUpWindow}`)
+
+  if (popUpWindow && popUpWindow.classList.contains('bottom-[-150px]')) {
+    popUpWindow.classList.add('bottom-0', 'opacity-1');
+
+    console.log('clase botton-0 añadida');
+
+
+    setTimeout(() => {
+      popUpWindow.classList.remove('bottom-0');
+    }, 3000);
+  }
+}
+
