dd60fc0dab5601e6e33fdf9e71c7460415cd10d9
xpilasi <pilasiha@gmail.com>
2024-10-19 21:23:42 +0200

    popup ok

--- js/form.js
+++ js/form.js
@@ -15,7 +15,7 @@ const enviarFormulario = (event) => {
   esValido &= validarCampo(emailForm, /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
 
   if (esValido) {
-    console.log("Formulario válido. Enviando datos...");
+   
     // enviar los datos al servidor, o dejar que el formulario se envíe
     showConfirmationPopUp()
     // event.target.submit(); // Solo si es válido se permite el envío  <-- Lo cancelo porque si se envia, no se ve el popup
@@ -52,20 +52,15 @@ document.querySelector("#email-form").addEventListener("keyup", (event) => {
 
 
 // Mostrar ventana emergente de confirmacion
-
-
 function showConfirmationPopUp() {
 
-  console.log(`Has pedido que se mostrara el popup de confirmacion. El elemento DOM de la ventana es: ${popUpWindow}`)
-
   if (popUpWindow && popUpWindow.classList.contains('bottom-[-150px]')) {
-    popUpWindow.classList.add('bottom-0', 'opacity-1');
-
-    console.log('clase botton-0 añadida');
-
+    popUpWindow.classList.remove('bottom-[-150px]');
+    popUpWindow.classList.add('bottom-[150px]','opacity-1', 'z-50');
 
     setTimeout(() => {
-      popUpWindow.classList.remove('bottom-0');
+      popUpWindow.classList.add('bottom-[-150px]');
+      popUpWindow.classList.remove('bottom-[150px]','opacity-1', 'z-50');
     }, 3000);
   }
 }
